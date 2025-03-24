cf905a5cb93ab945342cbcd20c925d8b95bca09e
Kirtana Ashok <kiashok@microsoft.com>
2025-03-24 10:04:49 -0700

    Ioctl calls to get scsi device path and disk numbers from lun
    Signed-off-by: Kirtana Ashok <kiashok@microsoft.com>

--- internal/windevice/devicequery.go
+++ internal/windevice/devicequery.go
@@ -3,13 +3,17 @@
 package windevice
 
 import (
+	"context"
 	"fmt"
 	"strings"
 	"unicode/utf16"
+	"unsafe"
 
 	"github.com/Microsoft/go-winio/pkg/guid"
+	"github.com/Microsoft/hcsshim/internal/log"
 	"github.com/Microsoft/hcsshim/internal/winapi"
 	"github.com/pkg/errors"
+	"golang.org/x/sys/windows"
 )
 
 const (
@@ -22,7 +26,64 @@ const (
 	_DEVPROP_TYPE_STRING_LIST uint32 = (_DEVPROP_TYPE_STRING | _DEVPROP_TYPEMOD_LIST)
 
 	_DEVPKEY_LOCATIONPATHS_GUID = "a45c254e-df1c-4efd-8020-67d146a850e0"
+
+	_IOCTL_SCSI_GET_ADDRESS = 0x41018
+
+	_IOCTL_STORAGE_GET_DEVICE_NUMBER = 0x2d1080
 )
+
+var (
+	// class GUID for devices with interface type Disk
+	// 53f56307-b6bf-11d0-94f2-00a0c91efb8b
+	devClassDiskGUID = guid.GUID{
+		Data1: 0x53f56307,
+		Data2: 0xb6bf,
+		Data3: 0x11d0,
+		Data4: [8]byte{0x94, 0xf2, 0x00, 0xa0, 0xc9, 0x1e, 0xfb, 0x8b},
+	}
+)
+
+// https://learn.microsoft.com/en-us/windows/win32/api/winioctl/ns-winioctl-storage_device_number
+type STORAGE_DEVICE_NUMBER struct {
+	DeviceType      uint64
+	DeviceNumber    uint64
+	PartitionNumber uint64
+}
+
+// SCSI_ADDRESS structure used with IOCTL_SCSI_GET_ADDRESS.
+// defined here: https://learn.microsoft.com/en-us/windows-hardware/drivers/ddi/ntddscsi/ns-ntddscsi-_scsi_address
+type SCSIAddress struct {
+	Length     uint32
+	PortNumber uint8
+	PathId     uint8
+	TargetId   uint8
+	Lun        uint8
+}
+
+// getScsiAddress retrieves the SCSI address from a given disk handle
+func getScsiAddress(handle windows.Handle) (*SCSIAddress, error) {
+	// Create a SCSI_ADDRESS structure to receive the address information
+	address := &SCSIAddress{}
+	address.Length = uint32(unsafe.Sizeof(SCSIAddress{}))
+
+	// Buffer for the returned data
+	var bytesReturned uint32
+
+	// Call DeviceIoControl with IOCTL_SCSI_GET_ADDRESS
+	err := windows.DeviceIoControl(
+		handle,
+		_IOCTL_SCSI_GET_ADDRESS,
+		nil, 0, // no input buffer
+		(*byte)(unsafe.Pointer(address)),
+		address.Length, &bytesReturned, nil)
+	if err != nil {
+		return nil, fmt.Errorf("DeviceIoControl failed with error: %w", err)
+	}
+	if bytesReturned <= 0 {
+		return nil, fmt.Errorf("DeviceIoControl returned %d bytes", bytesReturned)
+	}
+	return address, nil
+}
 
 // getDevPKeyDeviceLocationPaths creates a DEVPROPKEY struct for the
 // DEVPKEY_Device_LocationPaths property as defined in devpkey.h
@@ -93,6 +154,21 @@ func convertFirstNullTerminatedValueToString(buf []uint16) (string, error) {
 	return converted[:zerosIndex], nil
 }
 
+func convertNullSeparatedUint16BufToStringSlice(buf []uint16) []string {
+	result := []string{}
+	r := utf16.Decode(buf)
+	converted := string(r)
+	for {
+		i := strings.IndexRune(converted, '\u0000')
+		if i <= 0 {
+			break
+		}
+		result = append(result, string(converted[:i]))
+		converted = converted[i+1:]
+	}
+	return result
+}
+
 func GetChildrenFromInstanceIDs(parentIDs []string) ([]string, error) {
 	var result []string
 	for _, id := range parentIDs {
@@ -120,4 +196,82 @@ func getDeviceIDList(pszFilter *byte, ulFlags uint32) ([]string, error) {
 	}
 
 	return winapi.ConvertStringSetToSlice(buf)
+}
+
+func getDeviceHandleFromPath(devPath string) (windows.Handle, error) {
+	utf16Path, err := windows.UTF16PtrFromString(devPath)
+	if err != nil {
+		return windows.Handle(unsafe.Pointer(nil)), fmt.Errorf("failed to convert interface path [%s] to utf16: %w", devPath, err)
+	}
+
+	handle, err := windows.CreateFile(utf16Path, windows.GENERIC_READ|windows.GENERIC_WRITE,
+		windows.FILE_SHARE_READ|windows.FILE_SHARE_WRITE,
+		nil, windows.OPEN_EXISTING, 0, 0)
+	if err != nil {
+		return windows.Handle(unsafe.Pointer(nil)), fmt.Errorf("failed to get handle to interface path [%s]: %w", devPath, err)
+	}
+	return handle, nil
+}
+
+func getDeviceInterfaceList(ctx context.Context, controller, lun uint8) ([]string, error) {
+	// get a list of all disk device interfaces
+	interfaceListSize := uint32(0)
+	if err := winapi.CMGetDeviceInterfaceListSize(&interfaceListSize, &devClassDiskGUID, nil, 0); err != nil {
+		return nil, fmt.Errorf("failed to get size of disk device interface list: %w", err)
+	}
+
+	buf := make([]uint16, interfaceListSize)
+	if err := winapi.CMGetDeviceInterfaceList(&devClassDiskGUID, nil, &buf[0], interfaceListSize, 0); err != nil {
+		return nil, fmt.Errorf("failed to get disk device interface list: %w", err)
+	}
+
+	interfacePaths := convertNullSeparatedUint16BufToStringSlice(buf)
+	log.G(ctx).Debugf("disk device interface list size: %d", len(interfacePaths))
+	log.G(ctx).WithField("list", interfacePaths).Trace("disk device interfaces")
+
+	return interfacePaths, nil
+}
+
+// GetScsiDevicePathAndDiskNumberFromControllerLUN finds a storage device that has the matching
+// `controller` and `LUN` and returns its scsi device path and corresponding disk number.
+func GetScsiDevicePathAndDiskNumberFromControllerLUN(ctx context.Context, controller, LUN uint8) (string, uint64, error) {
+	interfacePaths, err := getDeviceInterfaceList(ctx, controller, LUN)
+	if err != nil {
+		return "", 0, nil
+	}
+
+	// go over each disk device interface and find out its LUN
+	for _, iPath := range interfacePaths {
+		handle, err := getDeviceHandleFromPath(iPath)
+		if err != nil {
+			return "", 0, err
+		}
+		defer windows.Close(handle)
+
+		scsiAddr, err := getScsiAddress(handle)
+		if err != nil {
+			return "", 0, fmt.Errorf("failed to get SCSI address for interface path [%s]: %w", iPath, err)
+		}
+
+		// TODO(ambarve): is comparing controller with port number the correct way?
+		if scsiAddr.Lun == LUN && scsiAddr.PortNumber == controller {
+			// get the disk number
+			var bytesReturned uint32
+			var deviceNumber STORAGE_DEVICE_NUMBER
+			if err := windows.DeviceIoControl(
+				handle,
+				_IOCTL_STORAGE_GET_DEVICE_NUMBER,
+				nil, 0, // No input buffer
+				(*byte)(unsafe.Pointer(&deviceNumber)),
+				uint32(unsafe.Sizeof(deviceNumber)),
+				&bytesReturned,
+				nil,
+			); err != nil {
+				return "", 0, err
+			}
+
+			return iPath, deviceNumber.DeviceNumber, nil
+		}
+	}
+	return "", 0, fmt.Errorf("no device found with controller: %d & LUN:%d", controller, LUN)
 }
